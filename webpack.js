69abe5910111681423beda1e59870a2d698eaf79
Liam Richardson <hello@discoliam.com>
2023-11-30 12:52:25 +0000

    Local fonts

--- webpack.js
+++ webpack.js
@@ -46,8 +46,8 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset',
+        test: /\.(png|jpe?g|gif|svg|webp|avif|jxl|woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
         generator: {
           filename: `images/[name][ext]`,
         },
